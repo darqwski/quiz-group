a9847574f112f9c43e83d9bbc63b173cb7bd1d93
darqwski <dariusz.cabala.pl@gmail.com>
2020-12-20 20:14:01 +0100

    [QG] - Create quiz validation

--- API/quiz/index.php
+++ API/quiz/index.php
@@ -11,6 +11,35 @@ function createQuiz() {
     $data = RequestAPI::getJSON();
     $questions = $data['questions'];
     $quiz = $data['quiz'];
+    if(!isset($data['quizName']) || !isset($data['quizDescription']) || !isset($data['quizCategory'])){
+        return Response::message("Quiz data is missing", 400);
+    }
+    if(count($questions) != 5){
+        return Response::message("Wrong number of questions", 400);
+
+    }
+    foreach ($questions as $question){
+        if(!isset($question['answers'])){
+            return Response::message("Question doesn't contain answers", 400);
+        }
+        if($question['answers'] != 4){
+            return Response::message("Question doesn't contain proper amount of answers", 400);
+        }
+        $onCorrectAnswer = 0;
+        foreach ($question['answers'] as $answer){
+            if(isset($answer['isCorrect'])){
+                $onCorrectAnswer++;
+            }
+        }
+        if($onCorrectAnswer == 0){
+            return Response::message("Question doesn't contain correct answer", 400);
+        }
+
+        if($onCorrectAnswer != 1){
+            return Response::message("Question doesn't contain proper amount of correct answer", 400);
+        }
+    }
+
     $quizId = PDOController::insertCommand("
     INSERT INTO `quizes` (
         `quizId`, `name`, `description`, `creatorId`, `isActive`, `categoryId`, `created`
@@ -23,9 +52,7 @@ function createQuiz() {
     'userId'=>$_SESSION['userId'],
     'quizCategory'=>$quiz['quizCategory']
     ]);
-    //TODO walidacja ilości pytań === 5,
-    //TODO walidacja ilość odpowiedzi per pytanie === 4,
-    //TODO walidacja ilość odpowiedzi poprawnych odpowiedzi per pytanie === 1,
+
     foreach ($questions as $question){
         $questionId=PDOController::insertCommand("
         INSERT INTO `questions` (`questionId`, `quizId`, `text`, `type`) 
